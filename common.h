d3fb1d2269bfef2c71d3913ea75b6912033e3564
rald <thecoconutnutisagiantnut@gmail.com>
2026-09-03 04:17:18 +0800

    OK

--- common.h
+++ common.h
@@ -1,11 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define BIBLE_CSV_FILE "/data/data/com.termux/files/home/projects/kjvak/kjvak.csv"
-
-#define BIBLE_INF_FILE "/data/data/com.termux/files/home/projects/kjvak/kjvak.inf"
-
-#define CONFIG_INI_FILE "/data/data/com.termux/files/home/projects/kjvak/config.ini"
+#define BIBLE_CSV_FILE      "/home/fria/projects/kjvak/kjvak.csv"
+#define BIBLE_INF_FILE      "/home/fria/projects/kjvak/kjvak.inf"
+#define CONFIG_INI_FILE     "/home/fria/projects/kjvak/config.ini"
 
 #define STRING_MAX 65536
 
